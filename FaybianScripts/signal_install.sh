f2a671274e30723aa6adc5d91acbe32e7a75db75
whimsy <whimsy@sylph.box>
2025-04-11 18:28:35 -0500

    update key

--- FaybianScripts/signal_install.sh
+++ FaybianScripts/signal_install.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
+set -e
 
-# Add Signal Desktop repository
+echo "Fetching and adding Signal Desktop GPG key..."
+sudo mkdir -p /usr/share/keyrings
+curl -fsSL https://updates.signal.org/desktop/apt/keys.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
 echo "Adding Signal Desktop repository..."
-sudo tee /etc/apt/sources.list.d/signal-desktop.list << EOL
-deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main
-EOL
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt stable main" | sudo tee /etc/apt/sources.list.d/signal-desktop.list
 
-# Retrieve GPG key for Signal Desktop repository
-GPG_FINGERPRINT="DBA36B5181D0C816F630E889D980A17457F6FB06"
-RECV_CMD="sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $GPG_FINGERPRINT"
-echo "Retrieving GPG key with fingerprint $GPG_FINGERPRINT"
-echo "Executing command: $RECV_CMD"
-$RECV_CMD
-
-# Update package list
 echo "Updating package list..."
 sudo apt update
 
-# Install Signal Desktop
 echo "Installing Signal Desktop..."
 sudo apt install signal-desktop #-y
-
-# Fix dependencies
-#echo "Fixing dependencies..."
-#sudo apt install -f
-
-# Launch Signal Desktop (optional)
-#echo "Starting Signal Desktop..."
-#signal-desktop &
