06d70c6d4a5921c20dd0b4219c46c2123cbc7088
whimsy <whimsy@sylph.box>
2025-04-11 21:37:51 -0500

    - Created  to install ProtonVPN appindicator dependencies - Installs Ubuntu packages required for ProtonVPN's system tray integration

---
+++ FaybianScripts/enable-protonvpn-notification-tray.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install dependencies for ProtonVPN notification tray icons
+sudo apt-get install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 indicator-application
+gsettings set org.gnome.shell enabled-extensions "['appindicatorsupport@rgcjonas.gmail.com']"
+#busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting GNOME Shell...")'
+
