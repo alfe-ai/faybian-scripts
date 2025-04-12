cd9909f53fb24b8372affa4a7a3b0396b28ccd5c
whimsy <whimsy@sylph.box>
2025-04-11 21:41:41 -0500

    modified:   enable-protonvpn-notification-tray.sh

--- FaybianScripts/enable-protonvpn-notification-tray.sh
+++ FaybianScripts/enable-protonvpn-notification-tray.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Install dependencies for ProtonVPN notification tray icons
-sudo apt-get install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 indicator-application
+#sudo apt-get install gnome-shell-extension-appindicator gir1.2-appindicator3-0.1 indicator-application
+sudo apt install libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
 #gsettings set org.gnome.shell enabled-extensions "['appindicatorsupport@rgcjonas.gmail.com']"
 #busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting GNOME Shell...")'
 
