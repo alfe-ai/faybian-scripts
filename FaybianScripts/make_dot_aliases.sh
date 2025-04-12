c3dcd7a0a9d24996f1e6e73ed9ef16d7da2c0b5d
whimsy <whimsy@sylph.box>
2025-04-12 01:02:06 -0500

    new file:   make_dot_aliases.sh

---
+++ FaybianScripts/make_dot_aliases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Creates the .alfe directory and symlinks for git
+
+mkdir -p "${HOME}/.alfe"
+if [ ! -L "${HOME}/.alfe/git" ]; then
+    ln -s "${HOME}/.fayra/Whimsical/git" "${HOME}/.alfe/git"
+fi
+
+if [ ! -L "${HOME}/git" ]; then
+    ln -s "${HOME}/.alfe/git" "${HOME}/git"
+fi
+
+echo "Directory ~/.alfe ensured. Symlinks ~/.alfe/git and ~/git created."
