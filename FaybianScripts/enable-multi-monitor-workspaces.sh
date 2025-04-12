e0225cdd91719582b24128da3746a23ecb72af2f
whimsy <whimsy@sylph.box>
2025-04-11 22:32:41 -0500

    Revert "	new file:   enable-multi-monitor-workspaces.sh"
    This reverts commit 589eee1620143aab675589135c0d2b4b3fbc2594.

--- FaybianScripts/enable-multi-monitor-workspaces.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt install gnome-tweaks -y
-#Open GNOME Tweaks from the application menu.
-#Navigate to Workspaces → Display Handling.
-#Set it to Workspaces span displays. This ensures that workspace switching applies to all monitors.
