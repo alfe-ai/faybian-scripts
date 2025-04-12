66bf1554f7f88c0518aeb71ac015f829f7b138c6
whimsy <whimsy@sylph.box>
2025-04-11 21:28:23 -0500

    modified:   enable-gnome-notification-tray.sh

--- FaybianScripts/enable-gnome-notification-tray.sh
+++ FaybianScripts/enable-gnome-notification-tray.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Install and enable AppIndicator extension for notification tray
-sudo apt-get install -y gnome-shell-extension-appindicator
+sudo apt-get install gnome-shell-extension-appindicator #-y
 gsettings set org.gnome.shell enabled-extensions "['appindicatorsupport@rgcjonas.gmail.com']" 
-busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting GNOME Shell...")'
+#busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting GNOME Shell...")'
