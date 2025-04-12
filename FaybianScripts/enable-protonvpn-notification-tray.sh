2b7d815ca686cfd1bfb6b62e1dc3e234d2efb8ab
whimsy <whimsy@sylph.box>
2025-04-11 22:00:37 -0500

    modified:   FaybianScripts/enable-protonvpn-notification-tray.sh

--- FaybianScripts/enable-protonvpn-notification-tray.sh
+++ FaybianScripts/enable-protonvpn-notification-tray.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Install dependencies for ProtonVPN notification tray icons
 sudo apt install libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
+
+# Ref: https://protonvpn.com/support/official-linux-vpn-ubuntu
