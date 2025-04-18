90072050b51a778c614cc0e41a432723efe36c37
lochner <nlochner@alfe.sh>
2025-04-17 21:32:27 -0500

    Update set_workspace_switch_hotkeys.sh

--- FaybianScripts/set_workspace_switch_hotkeys.sh
+++ FaybianScripts/set_workspace_switch_hotkeys.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 gsettings get org.gnome.desktop.wm.keybindings switch-to-workspace-right
 
 gsettings get org.gnome.desktop.wm.keybindings switch-to-workspace-left
 
 # For moving right
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Right', '<Alt><Control>Right']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Right', '<Alt><Control>Right', '<Alt><Super>Right']"
 
 # For moving left
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Left', '<Alt><Control>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Left', '<Alt><Control>Left', '<Alt><Super>Left']"
 
 gsettings get org.gnome.desktop.wm.keybindings switch-to-workspace-right
 
